cd7f58e7f439a3b7de746bb086b00b5736ad7d1d
VyacheslavKos <bouncer1984@mail.ru>
2020-10-30 23:33:45 +0500

    v. 1.1

--- app.py
+++ app.py
@@ -1,46 +1,42 @@
+import random
+
 from flask import Flask
 from flask import render_template
-from static.data import title
-from static.data import subtitle
-from static.data import description
-from static.data import departures
-from static.data import tours
-from random import randint
+
+from data.data import *
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def re_main():
+def main():
     # Выбираем произвольные 6 отелей
+    id_tours = []
     tours_random = {}
-    while len(tours_random) < 6:
-        random_tour = randint(1, len(tours))
-        if random_tour not in tours_random.keys():
-            tours_random[random_tour] = tours[random_tour]
+    for tour_id in tours:
+        id_tours.append(tour_id)
+    random.shuffle(id_tours)
+    for i in range(6):
+        tours_random[id_tours[i]] = tours[id_tours[i]]
     return render_template('index.html', title=title, tours_random=tours_random, subtitle=subtitle,
                            description=description, departures=departures)
 
 
 @app.route('/departures/<departure>/')
-def re_departure(departure):
+def re_departures(departure):
     # Фильтруем отели по привязке к месту вылета
     tours_dep = {}
     for key, value in tours.items():
-        if departure in value.values():
+        if departure == value['departure']:
             tours_dep[key] = value
     return render_template('departure.html', title=title, departure=departure, departures=departures,
                            tours_dep=tours_dep)
 
 
-@app.route('/tours/<int:id>/')
-def render_tours(id):
-    return render_template('tour.html', title=title, tour=tours[id], departures=departures)
-
-
-def base():
-    return render_template('base.html', title=title, departures=departures)
+@app.route('/tours/<int:ide>/')
+def re_tours(ide):
+    return render_template('tour.html', title=title, tour=tours[ide], departures=departures)
 
 
 if __name__ == '__main__':
